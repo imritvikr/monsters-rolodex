b88722a4ad832ab33d9d652dbc649371f04bcc17
Ramakant <2016meb1140@iitrpr.ac.in>
2020-08-05 23:41:21 +0530

    Updates

---
+++ precache-manifest.98d0bd64cf054ef0923f25fc0f610a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6402e27a5f04a95a6cc6bcbea7008e91",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d890937207785932f2fc",
+    "url": "/monsters-rolodex/static/css/main.cd1a115c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d890937207785932f2fc",
+    "url": "/monsters-rolodex/static/js/main.44bd5dd9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
